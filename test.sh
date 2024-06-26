d807d3921aeb96567cfa99c37767c75bc628946e
Christian Smith <smith.christian.william@gmail.com>
2024-06-26 11:49:34 -0400

    update test

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-cargo build; sudo target/debug/albion-chat-sniffer | stdbuf -oL grep 1234567890 > logs/say.log
+cargo build
+mkdir -p logs
+sudo target/debug/albion-chat-sniffer | stdbuf -oL grep 1234567890 > logs/out.log
 
